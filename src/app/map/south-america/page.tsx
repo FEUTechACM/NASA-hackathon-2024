7a6750ffb2220b6c088da7de443b6220b9f6fb24
Alpha Romer Coma <alpharomercoma@proton.me>
2024-10-06 19:25:31 +0800

    refactor: add initial south america

---
+++ src/app/map/south-america/page.tsx
@@ -0,0 +1,10 @@
+import SouthAmericaComponent from "@/components/map/south-america";
+const SouthAmericaMap = () => {
+    return (
+        <>
+            <SouthAmericaComponent />
+        </>
+    );
+};
+
+export default SouthAmericaMap;
--- src/components/map/south-america.tsx
+++ src/components/map/south-america.tsx
@@ -15,10 +15,10 @@ type Marker = {
 
 // Define the marker data with the correct type
 const markers: Marker[] = [
-    { position: [0, 0.5, 3], note: "Southeast Asia", image: "/placeholder.svg?height=200&width=300", story: "The Asia-Pacific region, which includes major countries such as China and India, is the world's largest emitter of carbon dioxide. This is due to rapid industrialization, increasing use of coal for electricity generation, and the development of the transportation industry. Urbanization and rising energy demand contribute greatly to carbon emissions in the region. However, much has been done for green innovation, such as investment in renewable energy sources and stricter environmental regulations. Countries in the area also invest in technological innovation to reduce emissions and mitigate climate change." },
-    { position: [-1, 0.5, 0], note: "China", image: "/placeholder.svg?height=200&width=300", story: "In China, rapid industrialization sweeps the nation into a haze of carbon emissions. Coal-fired power plants breathe life into factories and homes, as cities stretch and transportation hums. The relentless rise in energy demands paints the skies with layers of CO2. This is the tale of a land intertwined with its quest for progress." },
-    { position: [-4, 0.5, 0], note: "India and Pakistan", image: "/placeholder.svg?height=200&width=300", story: "In Southeast Asia, the tale of carbon emissions is deeply tied to transportation. As cities expand and economies grow, roads become crowded with cars, trucks, and motorcycles. The constant hum of engines and the plume of exhaust create a dense tapestry of air pollution. This vibrant region's struggle is to balance rapid development with the need to reduce transportation emissions, weaving a narrative of progress intertwined with environmental consciousness." },
-    { position: [-8, 0.5, 1.2], note: "Saudi Arabia", image: "/placeholder.svg?height=200&width=300", story: "In Saudi Arabia, oil flows through the veins of the nation, fueling an energy sector that powers industries and transports dreams. Vast reserves drive carbon emissions skyward as cities rise and energy needs swell. The deserts echo with the whispers of carbon, marking a land steeped in its fossil fuel legacy. This is the saga of an oil-rich kingdom's environment" },
+    { position: [0, 0.5, 3], note: "Brazil", image: "/placeholder.svg?height=200&width=300", story: "The Asia-Pacific region, which includes major countries such as China and India, is the world's largest emitter of carbon dioxide. This is due to rapid industrialization, increasing use of coal for electricity generation, and the development of the transportation industry. Urbanization and rising energy demand contribute greatly to carbon emissions in the region. However, much has been done for green innovation, such as investment in renewable energy sources and stricter environmental regulations. Countries in the area also invest in technological innovation to reduce emissions and mitigate climate change." },
+    { position: [-1, 0.5, 0], note: "Argentina", image: "/placeholder.svg?height=200&width=300", story: "In China, rapid industrialization sweeps the nation into a haze of carbon emissions. Coal-fired power plants breathe life into factories and homes, as cities stretch and transportation hums. The relentless rise in energy demands paints the skies with layers of CO2. This is the tale of a land intertwined with its quest for progress." },
+    { position: [-4, 0.5, 0], note: "Colobmia", image: "/placeholder.svg?height=200&width=300", story: "In Southeast Asia, the tale of carbon emissions is deeply tied to transportation. As cities expand and economies grow, roads become crowded with cars, trucks, and motorcycles. The constant hum of engines and the plume of exhaust create a dense tapestry of air pollution. This vibrant region's struggle is to balance rapid development with the need to reduce transportation emissions, weaving a narrative of progress intertwined with environmental consciousness." },
+    { position: [-8, 0.5, 1.2], note: "Peru", image: "/placeholder.svg?height=200&width=300", story: "In Saudi Arabia, oil flows through the veins of the nation, fueling an energy sector that powers industries and transports dreams. Vast reserves drive carbon emissions skyward as cities rise and energy needs swell. The deserts echo with the whispers of carbon, marking a land steeped in its fossil fuel legacy. This is the saga of an oil-rich kingdom's environment" },
 ];
 
 interface MarkerProps {
@@ -80,7 +80,7 @@ function Background() {
 function IsometricMap() {
     const [mapSize] = useState({ width: 19.20, height: 10.80 });
     const [selectedMarker, setSelectedMarker] = useState<HoverInfo | null>(null);
-    const texture = useLoader(THREE.TextureLoader, '/map/asia.png');
+    const texture = useLoader(THREE.TextureLoader, '/map/sa.png');
 
     const { camera } = useThree();
 
@@ -117,7 +117,7 @@ function IsometricMap() {
     );
 }
 
-export default function Asia() {
+export default function SouthAmericaComponent() {
     return (
         <div className="w-full h-screen bg-white">
             <Canvas shadows>
